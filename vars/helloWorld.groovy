25f1feda2242d17d9b6bc8523054c3bc1e134225
hyna42 <bylo@duck.com>
2026-07-27 15:47:42 +0200

    feat: add varibales

--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -1,5 +1,17 @@
 #!/usr/bin/env groovy
 import groovy.json.*
 def call(body) {
-  println("Hello world !!!")
+  //on définit une map qui permet de stocke les clé/valeurs
+  def mapVars = [:]
+  
+  //récuperer le body ensuite l'intégrer dans la map qu'on vient de définir
+  body.resolveStrategy = Closure.DELEGATE_FIRST
+  body.delegate = mapVars
+  body()
+
+  //on récupre les valeurs ensuite on les insère
+  def name = mapVars.name
+  def action = mapVars.action
+
+  println(name + " Borom album " + action + "Yalna Ko Yalla Yerem, Barki Cheikh Ibrahima" )
 }
